2c5ad7ebb362ca81ddb79e2d45cfb97b0c6d2d5a
sheks11 <110970118+sheks11@users.noreply.github.com>
2023-06-23 07:17:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let btn=document.getElementById("submit");
+let va=document.getElementById("colorSelect");
+
+btn.addEventListener("click",()=>{
+ // console.log(va.children.length);
+   if (va.hasChildNodes()) {
+     for(let i=0;i<va.children.length;i++){
+      if(va.children[i].innerHTML==va.value){
+         va.removeChild(va.children[i]);
+      }
+     }
+    }
+
+})
